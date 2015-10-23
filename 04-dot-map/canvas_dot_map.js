06deb344cb785fa9451b0b82fde6d4b15f3f5a28
Carlos Cabo <carloscabo@gmail.com>
2015-10-23 20:32:47 +0200

    Mapa con puntos ready

--- 04-dot-map/canvas_dot_map.js
+++ 04-dot-map/canvas_dot_map.js
@@ -1,25 +1,21 @@
 'use strict';
 
-Array.matrix = function(numrows, numcols, initial) {
-  var arr = [];
-  for (var i = 0; i < numrows; ++i) {
-    var columns = [];
-      for (var j = 0; j < numcols; ++j) {
-        columns[j] = initial;
-      }
-    arr[i] = columns;
-  }
-  return arr;
-};
-
 /* Global vars */
 var gV = {
-  radius: 60,
-  x: 0,
-  y: 0
+  cx: 0,
+  cy: 0,
+  map: {
+    offset_x: 0,
+    offset_y: 0,
+    w: 910,
+    h: 450
+  },
+  dots: {
+    radius: 4,
+    spacing: 0
+  }
 },
 cz1,
-buffer,
 matrix;
 
 function Dot () {
@@ -32,81 +28,87 @@ function Dot () {
 function Matrix(cols, rows) {
   var matrix = new Array(cols);
   for(var i = 0; i < matrix.length; i++) {
-    matrix[i] = new Array(cols);
+    matrix[i] = new Array(rows);
   }
   return matrix;
 }
 // var myMatrix = Matrix(4, 9);
 // myMatrix[3][5] = "booger";
 
-function createDotArray() {
-  var img = new Image();
-  img.onload = function(){
-    console.log(img.width);
-    var canvas = document.createElement('canvas');
-    canvas.width = img.width;
-    canvas.height = img.height;
-    var ctx = canvas.getContext('2d');
-    ctx.drawImage(img, 0, 0);
-
-    readImgData(canvas, ctx);
-  };
-  img.src = 'img/mapa_04.png';
-}
-
-function readImgData(canvas, ctx) {
-  var
-    w = canvas.width,
-    h = canvas.height,
-    imgd = ctx.getImageData(0, 0, w, h),
-    pix = imgd.data,
-    col = 0,
-    row = 0;
-
-  // Crea la matriz
-  matrix = Matrix(w, h);
-
-  // Recorre los pixels de 4 en 4
-  for (var i = 0, n = pix.length; i < n; i += 4) {
-    // pix[i  ]; // red
-    // pix[i+1]; // green
-    // pix[i+2]; // blue
-    // i+3 is alpha (the fourth element)
-
-    // Normalice X/Y
-    var
-      x = col / w,
-      y = row / h,
-      dot = new Dot(); // Creamos el punto
-
-    // Posotion normalized
-    dot.x = x;
-    dot.y = y;
-    // Color and size
-    if (pix[i] > 0) { // Red
-      dot.color = 1;
-      dot.size = 255 / pix[i];
-    } else if (pix[i+2] > 0) { // Blue
-      dot.color = 2;
-      dot.size = 255 / pix[i+2];
-    } else { // Black
-      dot.size = 0.1;
-    }
-    // Push dot to matrix
-    matrix[col][row] = dot;
-    // Increase cols / rows
-    col++;
-    if (col == w) {
-      col = 0;
-      row++;
-    }
-  }
-
-  // console.log(pix)
-}
-
+//
+// DOM READY!
+//
 $(document).ready(function() {
   // La magia aquí!
+
+  function createDotArray() {
+    var img = new Image();
+    img.onload = function(){
+      var canvas = document.createElement('canvas');
+      canvas.width = img.width;
+      canvas.height = img.height;
+      var ctx = canvas.getContext('2d');
+      ctx.drawImage(img, 0, 0);
+      readImgData(canvas, ctx);
+    };
+    img.src = 'img/mapa_04.png';
+  }
+
+  function readImgData(canvas, ctx) {
+    var
+      w = canvas.width,
+      h = canvas.height,
+      imgd = ctx.getImageData(0, 0, w, h),
+      pix = imgd.data,
+      col = 0,
+      row = 0;
+
+    // Crea la matriz
+    matrix = Matrix(w, h);
+
+    // Recorre los pixels de 4 en 4
+    for (var i = 0, n = pix.length; i < n; i += 4) {
+      // pix[i  ]; // red
+      // pix[i+1]; // green
+      // pix[i+2]; // blue
+      // i+3 is alpha (the fourth element)
+
+      // Normalice X/Y
+      var
+        x = col / w,
+        y = row / h,
+        dot = new Dot(); // Creamos el punto
+
+      // Posotion normalized
+      dot.x = x;
+      dot.y = y;
+      // Color and size
+      if (pix[i] > 0) { // Red
+        dot.color = 1;
+        dot.size = pix[i] / 255;
+      } else if (pix[i+2] > 0) { // Blue
+        dot.color = 2;
+        dot.size = pix[i+2] / 255;
+      } else { // Black
+        dot.size = 0.1;
+      }
+      // Push dot to matrix
+      matrix[col][row] = dot;
+      // Increase cols / rows
+      col++;
+      if (col === w) {
+        col = 0;
+        row++;
+      }
+    }
+
+    matrixReady()
+    // return matrix;
+  }
+
+  function matrixReady() {
+    cz1.start();
+  }
 
   createDotArray();
 
@@ -116,40 +118,47 @@ $(document).ready(function() {
 
   // HSL
   var palette = [
-    [322, 35, 59], // [3, 91, 68],  // Rojo [248,108,101],
-    [351, 46, 67], // [121, 35, 55],// Verde [98,180,99],
-    [54, 100, 47]  // [43, 79, 59]  // Amarillo [233,187,68]
+    [216,216,216], // Gris claro
+    [200,200,200], // Gris medio
+    [185,193, 23]  // Verde gonico
   ];
 
   // Canvas
   cz1.clear = function() {
-    this.fS = '#242424';
+    this.fS = '#ffffff';
     this.ctx.beginPath();
     this.ctx.rect(0, 0, this.w, this.h);
     this.ctx.fill();
   };
 
   cz1.beforeStart = function() {
-    gV.x = cz1.center.x;
-    gV.y = cz1.center.y;
+    gV.cx = cz1.center.x;
+    gV.cy = cz1.center.y;
   };
 
   cz1.beforeDraw = function() {
     this.clear();
-    gV.x = gV.x - 4;
-    // If plot leaves the screen in the left put it in the right
-    if (gV.x < (gV.radius * -1)) {
-      gV.x = cz1.w + gV.radius;
-    }
   };
 
   cz1.draw = function() {
-    cz1.sS = '#666';
-    cz1.fS = '#f00';
-    cz1.plot(gV.x, gV.y, gV.radius);
+    // cz1.sS = '#666';
+    cz1.plot(gV.cx, gV.cy, gV.radius);
+
+    for(var i = 0; i < matrix.length; i++) {
+      for(var j = 0; j < matrix[j].length; j++) {
+        // console.log(matrix[i][j]);
+        var dot = matrix[i][j];
+        var x = gV.map.offset_x + (dot.x * gV.map.w);
+        var y = gV.map.offset_y + (dot.y * gV.map.h);
+        // console.log('rgba('+palette[dot.color].join(',')+')');
+        cz1.fS = 'rgb('+palette[dot.color].join(',')+')';
+        cz1.plot( x, y, gV.dots.radius * dot.size);
+        // console.log(dot.size);
+      }
+    }
   };
 
-  cz1.start();
+
 
 });
 
