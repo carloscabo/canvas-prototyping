cd836f1dbe79f30e53037f265a7bae145cbab670
Carlos Cabo <carloscabo@gmail.com>
2017-08-21 20:43:51 +0200

    Overlapping is working

--- 10-dungeon-generator/canvas_dungeon_generator.js
+++ 10-dungeon-generator/canvas_dungeon_generator.js
@@ -6,16 +6,18 @@
 var gV = {
   radius: 768 / 2,
 
-  room_nuber: 40,
+  room_number: 90,
   max_room_w: 14,
-  min_room_w: 4,
+  min_room_w: 2,
   max_room_h: 16,
-  min_room_h: 6,
+  min_room_h: 2,
 
-  grid: 8, // Pixels
+  grid: 6, // Pixels
 
   x: 0,
   y: 0,
+
+  average_area: 0,
 
   overlapping: true
 },
@@ -27,7 +29,7 @@ $(document).ready(function() {
 
   // Inicializamos el canvas
   cz1 = new Canvaz('#cnvz');
-  // cz1.fullScreen();
+  cz1.fullScreen();
 
   // HSL
   var palette = [
@@ -40,7 +42,7 @@ $(document).ready(function() {
   cz1.clear = function() {
     this.fS = '#242424';
     this.ctx.beginPath();
-    this.ctx.rect( - gV.radius, - gV.radius, gV.radius * 2, gV.radius * 2);
+    this.ctx.rect(- cz1.w / 2, - cz1.h / 2, cz1.w, cz1.h);
     this.ctx.fill();
   };
 
@@ -56,17 +58,24 @@ $(document).ready(function() {
     cz1.ctx.translate(cz1.w / 2, cz1.h / 2);
 
     // Create rooms
-    for (var i = 0; i < gV.room_nuber; i++) {
+    for (var i = 0; i < gV.room_number; i++) {
       var
         // p1 = getRandomPointInEllipse (0, 0, gV.radius / 2, gV.radius / 2);
-        p1 = getRandomPointInEllipse(0, 0, gV.radius / 4, gV.radius / 4);
-      rooms.push( {
-        x: p1[0],
-        y: p1[1],
-        w: gV.min_room_w + Math.floor( Math.random() * ( gV.max_room_w - gV.min_room_w ) ),
-        h: gV.min_room_h + Math.floor( Math.random() * ( gV.max_room_h - gV.min_room_h ) )
-      } );
+        p1 = getRandomPointInEllipse(0, 0, gV.radius / 8, gV.radius / 8),
+        room = {
+          x: p1[0],
+          y: p1[1],
+          w: gV.min_room_w + Math.floor(Math.random() * (gV.max_room_w - gV.min_room_w)),
+          h: gV.min_room_h + Math.floor(Math.random() * (gV.max_room_h - gV.min_room_h))
+        };
+
+      room.area = room.w * room.h;
+      gV.average_area += room.area;
+      rooms.push( room );
     }
+
+    // Not iot realli the average
+    gV.average_area = gV.average_area / gV.room_number;
 
     // console.log( rooms );
   };
@@ -92,7 +101,7 @@ $(document).ready(function() {
             r2 = rooms[j];
 
           // If rooms overlap
-          if ( roomOverlapping(r1, r2) ) {
+          if ( gV.overlapping && roomOverlapping(r1, r2) ) {
             // console.log( 'overlaps' );
             var
               d = utilz.dist( r1.x, r1.y, r2.x, r2.y),
@@ -108,9 +117,9 @@ $(document).ready(function() {
             midpoint.y = (r1.y + r2.y) / 2;
 
             r1.x -= normal.x;
-            r1.y -= normal.y;
+            r1.y -= normal.y * 0.15;
             r2.x -= normal.x * -1;
-            r2.y -= normal.y * -1;
+            r2.y -= normal.y * -1 * 0.15;
           }
 
         }
@@ -169,6 +178,9 @@ function drawRoom( room ) {
   cz1.fS = 'rgba(45, 93, 180, 0.75)';
   cz1.sS = '#fff';
 
+  if  ( room.area > gV.average_area * 1.35 ) {
+    cz1.fS = 'rgba(180, 93, 45, 0.75)';
+  }
   cz1.rect(room.x, room.y, room.w * gV.grid, room.h * gV.grid );
   cz1.ctx.fill();
 }
